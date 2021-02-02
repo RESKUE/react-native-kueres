05198545f61243eaabae049a6205446f7e812307
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-02 22:13:30 +0100

    Add the auth context provider

---
+++ src/auth/AuthProvider.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AuthContext from './AuthContext';
+
+export default function AuthProvider({authService, children}) {
+  const [authStatus, setAuthStatus] = React.useState(null);
+
+  React.useEffect(() => {
+    const callback = (status) => setAuthStatus(status);
+    return authService.onStatusChanged(callback);
+  }, [authService]);
+
+  return (
+    <AuthContext.Provider value={{authService, authStatus}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
