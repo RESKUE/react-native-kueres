3e72e46a422a8bff62e739c9190d16be3fcd0dca
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-05 15:19:11 +0100

    Add DataSource and FetchPolicy enums

---
+++ src/network/DataSource.js
@@ -0,0 +1,1 @@
+export default Object.freeze({cache: 'cache', network: 'network'});
---
+++ src/network/FetchPolicy.js
@@ -0,0 +1,7 @@
+export default Object.freeze({
+  cacheFirst: 'cacheFirst',
+  cacheOnly: 'cacheOnly',
+  cacheAndNetwork: 'cacheAndNetwork',
+  networkOnly: 'networkOnly',
+  noCache: 'noCache',
+});
