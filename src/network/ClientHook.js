eea303c155d946b40c258b673f49d4cd1607ac1b
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-17 17:13:16 +0100

    Drop support for custom client hook clients

--- src/network/ClientHook.js
+++ src/network/ClientHook.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Cache from './Cache';
 import Client from './Client';
 
-export default function useClient(customClient = null) {
+export default function useClient() {
   const [result, setResult] = React.useState(null);
-  const client = React.useMemo(() => customClient || new Client(new Cache()), [
-    customClient,
-  ]);
+  const client = React.useMemo(() => new Client(new Cache()), []);
 
   React.useEffect(() => {
     function handleResponse(data, error, source) {
