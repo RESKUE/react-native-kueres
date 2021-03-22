e9679c38d05df28754791ad1bf5ba30b272c6688
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-22 17:31:55 +0100

    Add FancyGrid and FancyList item rendering tests

--- __tests__/FancyGrid.tests.js
+++ __tests__/FancyGrid.tests.js
@@ -1,8 +1,17 @@
 import 'react-native';
+import {Text} from 'react-native';
 import renderer from 'react-test-renderer';
 import React from 'react';
 import FancyGrid from '../src/components/FancyGrid';
 
-test('fancy grid renders correctly', () => {
+test('fancy grid renders correctly without items', () => {
   renderer.create(<FancyGrid />);
+});
+
+test('fancy grid renders correctly with items', () => {
+  function Item({data}) {
+    return <Text>{data}</Text>;
+  }
+  const dataList = ['text1', 'text2'];
+  renderer.create(<FancyGrid component={Item} dataList={dataList} />);
 });
--- __tests__/FancyList.tests.js
+++ __tests__/FancyList.tests.js
@@ -1,8 +1,17 @@
 import 'react-native';
+import {Text} from 'react-native';
 import renderer from 'react-test-renderer';
 import React from 'react';
 import FancyList from '../src/components/FancyList';
 
-test('fancy list renders correctly', () => {
+test('fancy list renders correctly without items', () => {
   renderer.create(<FancyList />);
+});
+
+test('fancy list renders correctly with items', () => {
+  function Item({data}) {
+    return <Text>{data}</Text>;
+  }
+  const dataList = ['text1', 'text2'];
+  renderer.create(<FancyList component={Item} data={dataList} />);
 });
