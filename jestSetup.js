e8b199f464e622804caf06cf636f156f356edf80
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-23 04:51:52 +0100

    Setup jest fetch mock

--- jestSetup.js
+++ jestSetup.js
@@ -1,5 +1,8 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import fetchMock from 'jest-fetch-mock';
 
 jest.useFakeTimers();
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+fetchMock.enableMocks();
