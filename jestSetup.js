f903a5154a8e3dedb293ba01c6b462064b911d32
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-28 21:56:41 +0200

    Mock the animated library

--- jestSetup.js
+++ jestSetup.js
@@ -5,4 +5,7 @@ jest.useFakeTimers();
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
 fetchMock.enableMocks();
